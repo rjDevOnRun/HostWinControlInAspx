92d3211f2a8f51596536663305a84d8c9e7bbe1b
RJ <34760817+rjDevOnRun@users.noreply.github.com>
2020-09-01 14:07:03 +0900

    Update RadViewUserControl.cs
    Added Mouse Move Event handler

--- RadViewUC/RadViewUserControl.cs
+++ RadViewUC/RadViewUserControl.cs
@@ -9,12 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AxSPPIDRADViewCtl;
 using System.Runtime.CompilerServices;
+using SPPIDRADViewCtl;
 
 namespace RadViewUC
 {
     public partial class RadViewUserControl: UserControl
     {
         public AxRADViewCtl412 RadOCX;
+        double newX; double newY;
+        double currentX; double currentY;
+        const double c_Factor = 8.0;
+        const double c_FactorZero = 0.0;
+        const double c_NegativeFactor = -8.0;
+        const CoordinateSpaceConstants RADSpaceCoordSystem =
+                    CoordinateSpaceConstants.CoordinateSpacePixel;
 
         //// 1: Delegates
         //public delegate void RadFitEventHandler(object source, EventArgs args);
@@ -31,11 +39,50 @@ namespace RadViewUC
             this.RadOCX = this.axRADViewCtl4121;
             this.RadOCX.MouseScroll += RadOCX_MouseScroll;
             this.RadOCX.DblClick += RadOCX_DblClick;
+            this.RadOCX.MouseMoveEvent += RadOCX_MouseMoveEvent;
             this.SizeChanged += RadViewUserControl_SizeChanged;
 
             this.RadOCX.FileName = @"D:\SPPIDORCL\Plant\00\01\T-00-01-001.pid";
             //// 3: Raise Events
             //OnRadFitView();
+        }
+        
+        private void RadOCX_MouseMoveEvent(object sender, _DRadViewerEvents_MouseMoveEvent e)
+        {
+            if (axRADViewCtl4121.Visible.Equals(false))
+                return;
+
+            // View Panning
+            newX = Convert.ToDouble(e.x);
+            newY = Convert.ToDouble(e.y);
+
+            // Check Mouse-Middle button clicked then do View-Pan...
+            if (e.button == 4)
+            {
+                this.axRADViewCtl4121.CtlCursor = CursorTypeConstants.CursorPan;
+                if (currentX < newX)
+                {
+                    this.axRADViewCtl4121.Pan(RADSpaceCoordSystem, c_Factor, c_FactorZero);
+                }
+                else if (currentX > newX)
+                {
+                    this.axRADViewCtl4121.Pan(RADSpaceCoordSystem, c_NegativeFactor, c_FactorZero);
+                }
+                else if (currentY < newY)
+                {
+                    this.axRADViewCtl4121.Pan(RADSpaceCoordSystem, c_FactorZero, c_Factor);
+                }
+                else if (currentY > newY)
+                {
+                    this.axRADViewCtl4121.Pan(RADSpaceCoordSystem, c_FactorZero, c_NegativeFactor);
+                }
+            }
+
+            this.axRADViewCtl4121.CtlCursor = CursorTypeConstants.CursorLocate;
+
+            currentX = Convert.ToDouble(e.x);
+            currentY = Convert.ToDouble(e.y);
+
         }
 
         private void RadViewUserControl_SizeChanged(object sender, EventArgs e)
